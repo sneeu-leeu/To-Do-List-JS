8db1e7809e54b09969808e57e78329e0d4add163
sneeu-leeu <sa070197@gmail.com>
2021-07-22 15:12:30 +0200

    Add drag&drop file and JS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 /* eslint-disable no-unused-vars */
 import _ from 'lodash';
 import './style.css';
+import { dragStart, dragOver, drop } from './drag.js';
+
+const taskList = document.getElementById('js-todo-list');
 
 const todoList = [
   {
@@ -20,23 +23,60 @@ const todoList = [
   },
 ];
 
-function render() {
-  const taskList = document.getElementById('js-todo-list');
+function render(tasks) {
   taskList.innerHTML = '';
-  const sortedTask = todoList.sort((a, b) => a.index - b.index);
-  sortedTask.forEach((task) => {
+
+  tasks.forEach((task) => {
     const listItem = document.createElement('li');
+    listItem.draggable = true;
     listItem.classList.add('border', 'flex', 'height', 'pad-left', 'todo-item');
-    const spanItem = document.createElement('span');
-    const checkBox = document.createElement('input');
-    checkBox.type = 'checkbox';
-    checkBox.classList.add('check');
-    const menu = document.createElement('i');
-    menu.classList.add('fa', 'fa-ellipsis-v');
-    spanItem.append(checkBox, task.description);
-    listItem.append(spanItem, menu);
-    taskList.append(listItem);
+  });
+  listItem.id = task.index;
+
+  const spanItem = document.createElement('span');
+  const textElement = document.createElement('p');
+
+  textElement.innerText = task.description;
+  textElement.classList.add('d-inline');
+  const menu = document.createElement('i');
+  menu.classList.add('fa', 'fa-ellipsis-v', 'text-secondary');
+
+  listItem.append(spanItem, menu);
+  taskList.append(listItem);
+
+  listItem.addEventListener('dragover', dragOver);
+  listItem.addEventListener('dragstart', dragStart);
+  const checkBox = document.createElement('input');
+  checkBox.type = 'checkbox';
+  checkBox.classList.add('me-2');
+
+  if (task.completed) {
+    textElement.classList.add('strike', 'text-muted');
+    checkBox.checked = true;
+  }
+  spanItem.append(checkBox, textElement);
+  checkBox.addEventListener('change', () => {
+    updateStatus(textElement, checkBox, task);
   });
 }
+
+
+
+
+//   const sortedTask = todoList.sort((a, b) => a.index - b.index);
+//   sortedTask.forEach((task) => {
+//     const listItem = document.createElement('li');
+//     listItem.classList.add('border', 'flex', 'height', 'pad-left', 'todo-item');
+//     const spanItem = document.createElement('span');
+//     const checkBox = document.createElement('input');
+//     checkBox.type = 'checkbox';
+//     checkBox.classList.add('check');
+//     const menu = document.createElement('i');
+//     menu.classList.add('fa', 'fa-ellipsis-v');
+//     spanItem.append(checkBox, task.description);
+//     listItem.append(spanItem, menu);
+//     taskList.append(listItem);
+//   });
+// }
 
 document.addEventListener('DOMContentLoaded', render);
