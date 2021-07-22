f8c75f717608350f6fb53e2bdd0fd63af11d8265
sneeu-leeu <sa070197@gmail.com>
2021-07-22 16:28:11 +0200

    Create deletaTAsk function

--- src/add_remove.js
+++ src/add_remove.js
@@ -9,3 +9,13 @@ function addNewTask() {
   document.getElementById('task-description').value = '';
   localStorage.setItem('tasks', JSON.stringify(taskArray));
 }
+
+function deleteTask(task) {
+  const taskArray = JSON.parse(localStorage.getItem('tasks'));
+  const sortedTasks = taskArray.sort((a, b) => a.index - b.index);
+  sortedTasks.splice(task.index - 1, 1);
+  sortedTasks.forEach((task, index) => {
+    task.index = index + 1;
+  });
+  localStorage.setItem('tasks', JSON.stringify(sortedTasks));
+}
