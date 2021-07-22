9225b0ed413d9baaf1d675f30fe998239ee9f7ae
sneeu-leeu <sa070197@gmail.com>
2021-07-22 16:35:31 +0200

    Create clear All completed fx

--- src/add_remove.js
+++ src/add_remove.js
@@ -42,3 +42,18 @@ function editTask(e, listItem, textElement, spanItem, allTasks, task, populate) 
 
   inputElement.focus();
 }
+
+function clearAllCompleted() {
+  const taskArray = JSON.parse(localStorage.getItem('tasks'));
+  const filteredTaskArray = taskArray.filter((task) => task.completed === false);
+  const sortedTasks = filteredTaskArray.sort((a, b) => a.index - b.index);
+
+  sortedTasks.forEach((task, index) => {
+    task.index = index;
+  });
+  localStorage.setItem('tasks', JSON.stringify(sortedTasks));
+}
+
+module.exports = {
+  addNewTask, editTask, deleteTask, clearAllCompleted,
+};
