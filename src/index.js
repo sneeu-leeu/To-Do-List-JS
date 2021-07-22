0fe9813222cd72e1def0c4971f32d4f2ec35cc96
sneeu-leeu <sa070197@gmail.com>
2021-07-22 13:59:52 +0200

    Add render function to create toDo items in ul

--- src/index.js
+++ src/index.js
@@ -1,2 +1,38 @@
-import _ from 'lodash';
-import './style.css';
+import './css/styles.css';
+
+const todoList = [
+  {
+
+  },
+  {
+
+  },
+];
+
+function render() {
+  const taskList = document.getElementById('js-todo-list');
+  taskList.innerHTML = '';
+  const sortedTask = todoList.sort((a, b) => a.index - b.index);
+  sortedTask.forEach((task) => {
+    const listItem = document.createElement('li');
+
+    const spanItem = document.createElement('span');
+  });
+}
+
+// const taskList = document.getElementById('task-list');
+//   taskList.innerHTML = '';
+//   const sortedTask = tasks.sort((a, b) => a.index - b.index);
+//   sortedTask.forEach((task) => {
+//     const listItem = document.createElement('li');
+//     listItem.classList.add('border-bottom', 'p-3', 'pt-2', 'pb-2', 'd-flex', 'flex-row', 'justify-content-between', 'align-items-center');
+//     const spanItem = document.createElement('span');
+//     const checkBox = document.createElement('input');
+//     checkBox.type = 'checkbox';
+//     checkBox.classList.add('me-2');
+//     const menu = document.createElement('i');
+//     menu.classList.add('fa', 'fa-ellipsis-v', 'text-secondary');
+//     spanItem.append(checkBox, task.description);
+//     listItem.append(spanItem, menu);
+//     taskList.append(listItem);
+//   });
