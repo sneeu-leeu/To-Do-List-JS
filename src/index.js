182cf08a73400da4fca1ed150235af0a855d2e9f
sneeu-leeu <sa070197@gmail.com>
2021-07-22 14:14:41 +0200

    Add list item inner elements

--- src/index.js
+++ src/index.js
@@ -15,24 +15,17 @@ function render() {
   const sortedTask = todoList.sort((a, b) => a.index - b.index);
   sortedTask.forEach((task) => {
     const listItem = document.createElement('li');
-
+    listItem.classList.add('border flex height pad-left');
     const spanItem = document.createElement('span');
+    const checkBox = document.createElement('input');
+    checkBox.type = 'checbox';
+    checkBox.classList.add('check');
+    const menu = document.createElement('i');
+    menu.classList.add('fa', 'fa-ellipsis-v');
+    spanItem.append(checkBox, task.description);
+    listItem.append(spanItem, menu);
+    todoList.append(listItem);
   });
 }
 
-// const taskList = document.getElementById('task-list');
-//   taskList.innerHTML = '';
-//   const sortedTask = tasks.sort((a, b) => a.index - b.index);
-//   sortedTask.forEach((task) => {
-//     const listItem = document.createElement('li');
-//     listItem.classList.add('border-bottom', 'p-3', 'pt-2', 'pb-2', 'd-flex', 'flex-row', 'justify-content-between', 'align-items-center');
-//     const spanItem = document.createElement('span');
-//     const checkBox = document.createElement('input');
-//     checkBox.type = 'checkbox';
-//     checkBox.classList.add('me-2');
-//     const menu = document.createElement('i');
-//     menu.classList.add('fa', 'fa-ellipsis-v', 'text-secondary');
-//     spanItem.append(checkBox, task.description);
-//     listItem.append(spanItem, menu);
-//     taskList.append(listItem);
-//   });
+document.addEventListener('DOMContentLoaded', render);
