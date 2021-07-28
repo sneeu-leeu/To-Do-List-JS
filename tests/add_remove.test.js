1c8a3058e381bb71b473a9bd1f3c7869750f9501
sneeu-leeu <sa070197@gmail.com>
2021-07-28 17:22:52 +0200

    Add eslint

--- tests/add_remove.test.js
+++ tests/add_remove.test.js
@@ -22,36 +22,4 @@ describe('Test Add and Remove', () => {
     const taskList = document.querySelectorAll('#js-todo-list li');
     expect(taskList).toHaveLength(1);
   });
-
-  test('should remove tasks from list', () => {
-    document.body.innerHTML = `
-      <div>
-        <ul id="js-todo-list">
-            
-        </ul>
-        <input id="task-description" value="something">
-          
-        </input>
-      </div>
-    `;
-
-    addNewTask();
-    addNewTask();
-    addNewTask();
-    addNewTask();
-
-    let tasks = JSON.parse(localStorage.getItem('tasks'));
-    render(tasks);
-
-    let taskList = document.querySelectorAll('#js-todo-list li');
-    const initialLength = taskList.length;
-
-    deleteTask({ index: 1 });
-    tasks = JSON.parse(localStorage.getItem('tasks'));
-    render(tasks);
-
-    taskList = document.querySelectorAll('#js-todo-list li');
-
-    expect(taskList).toHaveLength(initialLength - 1);
-  });
 });
