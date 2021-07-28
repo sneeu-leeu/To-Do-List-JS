c7cb562a13e03ed39790947090c53dc6df20bb7c
Caleb Anderson <buyananderson@gmail.com>
2021-07-28 17:45:28 +0200

    Test the remove tast functionnality

--- tests/add_remove.test.js
+++ tests/add_remove.test.js
@@ -1,7 +1,6 @@
 const { addNewTask, deleteTask } = require('./test_files/add_remove.js');
 const { render } = require('./test_files/index.js');
 
-
 describe('Test Add and Remove', () => {
   test('should add tasks to list', () => {
     document.body.innerHTML = `
@@ -21,5 +20,27 @@ describe('Test Add and Remove', () => {
 
     const taskList = document.querySelectorAll('#js-todo-list li');
     expect(taskList).toHaveLength(1);
+  });
+
+  test('should remove task to List', () => {
+    document.body.innerHTML = `
+      <div>
+        <ul id="js-todo-list">
+            
+        </ul>
+        <input id="task-description" value="something">
+          
+        </input>
+      </div>
+    `;
+    addNewTask();
+    addNewTask();
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+    render(tasks);
+    let taskList = document.querySelectorAll('#js-todo-list li');
+    const initialLength = taskList.length;
+    deleteTask({ index: 1 });
+    taskList = document.querySelectorAll('#js-todo-list li');
+    expect(taskList).toHaveLength(initialLength - 1);
   });
 });
