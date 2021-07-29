f5c7a4c351ef3d00fec50fe23c11cae3c7003167
Caleb Anderson <buyananderson@gmail.com>
2021-07-29 15:32:34 +0200

    Create the file for testing the editTask

---
+++ tests/editTask.test.js
@@ -0,0 +1,7 @@
+const { updateStatus } = require('./test_files/box_status.js');
+const { render } = require('./test_files/index.js');
+const { addNewTask } = require('./test_files/add_remove.js');
+
+describe('Edit the task', () => {
+  test('Should Edit the task', () => {});
+});
